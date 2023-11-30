c7d2388b8b2ace66ad9b822b8a5fa8001a9a787c
gabecloud94 <gabecloud94@gmail.com>
2023-11-29 20:42:43 -0700

    Update to Auth Form

--- app/auth-form.tsx
+++ app/auth-form.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/utils/database.types'
+import { createClient } from '@supabase/supabase-js'
 
 
 export default function AuthForm() {
-  const supabase = createClientComponentClient<Database>()
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,)
 
   return (
     <Auth
